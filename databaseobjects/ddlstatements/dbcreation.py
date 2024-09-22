6fdc71b3a150323b5aca7d9fb3cd36a408bbd131
Luis Santiago Maya Restrepo <55595208+SantiagoMaya10@users.noreply.github.com>
2024-09-22 08:39:15 -0500

    se elimina confoguración de sql lite porque ya usaré una bd noral

--- databaseobjects/ddlstatements/dbcreation.py
+++ databaseobjects/ddlstatements/dbcreation.py
@@ -1,2 +1,1 @@
-create_dummy_table = """CREATE TABLE IF NOT EXISTS ping(ping_field text)"""
 insert_dummy_value = """INSERT INTO ping (ping_field) values("hola mundo")"""
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from fasthtml.common import Main, FastHTML, H1, P, A, Form, Button, serve, Title
-from databaseobjects.dbconfig import SqLiteConnector
+from fasthtml.common import FastHTML, serve
 from  views.homepage import build_home_page
 import os
 
@@ -8,16 +7,6 @@ app = FastHTML()
 @app.get("/")
 def home():
     return build_home_page()
-
-@app.get("/dummy")
-def increment():
-
-    db_conf = SqLiteConnector()
-    conn = db_conf.get_db_connection()
-    with conn:
-        data = conn.cursor().execute("select * from ping")
-
-    return P(data.fetchone())
 
 @app.get("/current-scope")
 def getCurrentScope():
